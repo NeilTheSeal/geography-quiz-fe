d29e61c7268187b09a3b1fe472e653c0e8c1bf33
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-06-06 13:27:07 -0600

    Add spec for previous scores on dashboard

--- spec/features/previous_scores_spec.rb
+++ spec/features/previous_scores_spec.rb
@@ -2,15 +2,22 @@ require 'rails_helper'
 
 RSpec.describe 'previous_scores' do
   before(:each) do
-    @user = create(:user, user_id: "1")
+    @user_1 = create(:user, user_id: "1")
+    @user_2 = create(:user, user_id: "2")
+    @user_3 = create(:user, user_id: "3")
+
   end
 
   it 'shows the previous scores' do
-    page.set_rack_session(user_id: @user.user_id, name: @user.name)
+    page.set_rack_session(user_id: @user_1.user_id, name: @user_1.name)
     visit "/dashboard"
 
     within '#previous-scores' do
-      expect(page).to have_css(".previous-score", count: 4)
+      expect(page).to have_content("20 / 20")
+      expect(page).to have_content("18 / 20")
+      expect(page).to have_content("16 / 20")
+      expect(page).to have_content("14 / 20")
+      expect(page).to have_css(".entry", count: 4)
     end
   end
 end
