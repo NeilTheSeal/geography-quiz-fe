2f5266f28c6ccc9819b445c46437605210a3692f
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-06-06 13:22:52 -0600

    remove vcr config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -75,13 +75,6 @@ Shoulda::Matchers.configure do |config|
   end
 end
 
-VCR.configure do |config|
-  config.cassette_library_dir = "spec/fixtures/vcr_cassettes"
-  config.hook_into :webmock
-  config.default_cassette_options = { re_record_interval: 7.days }
-  config.configure_rspec_metadata!
-  config.default_cassette_options = { decode_compressed_response: true }
-end
 
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
