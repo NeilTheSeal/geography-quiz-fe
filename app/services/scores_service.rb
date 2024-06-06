d40321144e1f3bf654a7a6ac7cad4f642a8d5a3d
NeilTheSeal <neil.hendren@gmail.com>
2024-06-06 15:56:31 -0600

    Refactor scores service

--- app/services/scores_service.rb
+++ app/services/scores_service.rb
@@ -9,13 +9,7 @@ class ScoresService
   end
 
   def previous_scores
-    conn = Faraday.new(url: @host) do |faraday|
-      faraday.headers["Accept"] = "application/json"
-      faraday.params["user_id"] = @user_id
-    end
-
-    response = conn.get("/api/v0/previous-scores")
-    json = JSON.parse(response.body, symbolize_names: true)
+    json = previous_scores_as_json
 
     json[:data].map do |score_data|
       {
@@ -26,6 +20,18 @@ class ScoresService
   end
 
   def self.high_scores
+    json = high_scores_as_json
+
+    json[:data].map do |score_data|
+      {
+        number_correct: score_data[:attributes][:number_correct],
+        name: User.find_by(user_id: score_data[:attributes][:user_id]).name,
+        created_at: score_data[:attributes][:created_at]
+      }
+    end
+  end
+
+  def self.high_scores_as_json
     host = if ENV["RAILS_ENV"] == "production"
              "https://ancient-plains-68209-663b50393b93.herokuapp.com"
            else
@@ -37,14 +43,18 @@ class ScoresService
     end
 
     response = conn.get("/api/v0/high-scores")
-    json = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 
-    json[:data].map do |score_data|
-      {
-        number_correct: score_data[:attributes][:number_correct],
-        name: User.find_by(user_id: score_data[:attributes][:user_id]).name,
-        created_at: score_data[:attributes][:created_at]
-      }
+  private
+
+  def previous_scores_as_json
+    conn = Faraday.new(url: @host) do |faraday|
+      faraday.headers["Accept"] = "application/json"
+      faraday.params["user_id"] = @user_id
     end
+
+    response = conn.get("/api/v0/previous-scores")
+    JSON.parse(response.body, symbolize_names: true)
   end
 end
