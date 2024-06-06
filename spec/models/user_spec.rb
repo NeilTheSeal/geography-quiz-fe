750b4636cb7a5708b194e124d14bb321d7e87a55
Rodrigo Chavez <Rodrigo103004@gmail.com>
2024-06-06 13:27:56 -0600

    test for the validates and to show the previous scores

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :user_id }
+  it { should validate_presence_of :name }
+
+  it "shows previous scores" do 
+    user_1 = create(:user, user_id: "1")
+    user_2 = create(:user, user_id: "2")
+    user_3 = create(:user, user_id: "3")
+
+    expect(user_1.previous_scores.first[:number_correct]).to eq(20)
+    expect(user_1.previous_scores.last[:number_correct]).to eq(14)
+  end
 end
